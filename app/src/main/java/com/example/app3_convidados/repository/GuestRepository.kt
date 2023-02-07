cfb04ef4019a672dda26fdb0ae9ddc910db57a86
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-07 18:14:26 -0300

    Inserido a função update na classe GuestRepository

--- app/src/main/java/com/example/app3_convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/app3_convidados/repository/GuestRepository.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import com.example.app3_convidados.constants.DataBaseConstants
 import com.example.app3_convidados.model.GuestModel
+import java.sql.DatabaseMetaData
 
 class GuestRepository private constructor(context: Context){
 
@@ -35,7 +36,22 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update(){
+    fun update(guest: GuestModel):Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        }catch (e: Exception){
+            false
+        }
 
     }
 
