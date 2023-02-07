6b2ccc1d5cde601e1555de83b856b57d4f93dcce
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-07 16:15:34 -0300

    Criação da tela para adicionar novo convidado atrelada ao FAB

--- app/src/main/java/com/example/app3_convidados/GuestFormActivity.kt
+++ app/src/main/java/com/example/app3_convidados/GuestFormActivity.kt
@@ -2,10 +2,29 @@ package com.example.app3_convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.app3_convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Dessa maneira a viewModel está atrelada a activity e quando morre a activity morre a viewModel
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_save){
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/app3_convidados/GuestFormViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.app3_convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+
+
+}
